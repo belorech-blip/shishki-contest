e3aa9d0c386183b99e696e5f15a2a189cc5af19a
belorech-blip <belorech@icloud.com>
2026-07-10 23:11:13 +0500

    Add publication endpoint placeholder

---
+++ backend/shishki_api/add-publication.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+json_response([
+    'success' => false,
+    'message' => 'Endpoint add-publication.php пока не реализован в стабильной версии'
+], 501);
